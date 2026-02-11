6219f4cf01bc92dcac74dd9b59d187a577018113
Ivan Ristic <ivan.ristic@global-engineering-technologies.com>
2026-02-11 10:10:55 +0100

    fix

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -7,5 +7,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("IISDeployExtension")]
 [assembly: AssemblyCopyright("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.3.0")]
+[assembly: AssemblyFileVersion("1.0.3.0")]
